ab186d28b9d1f07a304facfcd52f0b34ec9209ae
newboy <newboy@newboy.com>
2025-11-16 17:19:49 +0800

    完成别名联想设置

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<script setup></script>
+<script setup>
+  //这个配置项只做联想提示
+  import HelloWorld from '@/components/HelloWorld.vue'
+</script>
 
 <template>
   <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  
 </template>
 
 <style scoped></style>
